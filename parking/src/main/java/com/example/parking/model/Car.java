e539b3238494e91013ba45be1ba227a5b8beac22
yejin087 <zinyizhen8@gmail.com>
2022-05-23 09:47:20 +0900

    Car, Member 테이블 수정
    Member 테이블과 Car 테이블 연관, 일대 다

--- parking/src/main/java/com/example/parking/model/Car.java
+++ parking/src/main/java/com/example/parking/model/Car.java
@@ -1,7 +1,5 @@
 package com.example.parking.model;
 
-
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -12,12 +10,12 @@ import lombok.Data;
 @Data
 @Entity
 public class Car {
-	@Id
-	private String carNum; //차량번호
-	private String carName; //차량이름
-	private String carType; //차량타입(소형,대형)
+    @Id
+    private String carNum; // 차량번호
+    private String carName; // 차량이름
+    private String carType; // 차량타입(소형,대형)
 
-	@ManyToOne
-	@JoinColumn(name = "username")
-	private Member member;
+    @ManyToOne
+    @JoinColumn(name = "username")
+    private Member member;
 }
--- parking/src/main/java/com/example/parking/model/Member.java
+++ parking/src/main/java/com/example/parking/model/Member.java
@@ -1,51 +1,54 @@
 package com.example.parking.model;
 
-
 import java.util.List;
 
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import com.example.parking.exception.ValidationGroups;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
-//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-//@DiscriminatorColumn(name = "DTYPE")
+@Entity
 public class Member {
-	
-	@Id
-//	@NotNull(message = "아이디는 필수 값입니다.")
-	private String username; //아이디
-	
-//	@NotNull(message = "비밀번호는 필수 값입니다.")
-//	@Size(min = 4, max = 16, message = "길이는 4 ~ 16자 사이어야 합니다.")
-//	@Pattern(regexp = "^([A-Za-z])+([0-9])+$", message = "대소문자와 숫자의 조합이어야 합니다.")
-	private String password; // 비밀번호
 
-	private String  name; // 이름
-	
-//	@NotNull(message = "전화번호는 필수 값입니다.")
-//	@Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
-	private String tel; // 연락처
-	
-//	@NotNull(message = "이메일은 필수 값입니다.")
-//	@Pattern(regexp = "[a-zA-z0-9]+@[a-zA-z]+[.]+[a-zA-z.]+", message="example@example.com 형식으로 입력해야합니다.")
-	private String email;
-	
-	private String bizNum; // 사업자번호(사업자인경우)
-	
-	private String gubun; // 구분 : 사업자, 개인
-	private String role; // 권한
-	
-	@OneToMany(mappedBy = "Car")
-	private List<Car> car;
+    @Id
+    @NotBlank(message = "아이디를 입력하세요.", groups = ValidationGroups.NotEmptyGroup.class)
+    private String username; // 아이디
+
+    @NotBlank(message = "비밀번호를 입력하세요.", groups = ValidationGroups.NotEmptyGroup.class)
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}", message = "비밀번호는 영문과 숫자 조합으로 8 ~ 16자리까지 가능합니다.", groups = ValidationGroups.PatternCheckGroup.class)
+    private String password; // 비밀번호
+
+    @NotBlank(message = "이름을 입력하세요", groups = { ValidationGroups.NotEmptyGroup.class,
+            ValidationGroups.UpdateCheckGroup.class })
+    private String name; // 이름
+
+    @NotBlank(message = "전화번호를 입력하세요.", groups = { ValidationGroups.NotEmptyGroup.class,
+            ValidationGroups.UpdateCheckGroup.class })
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.", groups = {
+            ValidationGroups.PatternCheckGroup.class, ValidationGroups.UpdateCheckGroup.class })
+    private String tel; // 연락처
+
+    @NotBlank(message = "이메일을 입력하세요.", groups = { ValidationGroups.NotEmptyGroup.class,
+            ValidationGroups.UpdateCheckGroup.class })
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}", message = "올바르지 않은 이메일 형식입니다.", groups = {
+            ValidationGroups.PatternCheckGroup.class, ValidationGroups.UpdateCheckGroup.class })
+    private String email;
+
+    // 패턴만 적용?
+    private String bizNum; // 사업자번호(사업자인경우)
+
+    private String gubun; // 구분 : 사업자, 개인
+    private String role; // 권한
+
+    @OneToMany(mappedBy = "member")
+    private List<Car> car;
 }
-
